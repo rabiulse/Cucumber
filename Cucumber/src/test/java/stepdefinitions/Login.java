f547486aa0a5e475c83bbb97a86090d0fda63c6c
rabiulse <rabiul.print@gmail.com>
2024-04-27 12:23:39 +0530

    commit 27thApril

--- Cucumber/src/test/java/stepdefinitions/Login.java
+++ Cucumber/src/test/java/stepdefinitions/Login.java
@@ -1,13 +1,24 @@
 package stepdefinitions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.*;
 
 public class Login {
 	
+	public static WebDriver driver;
+	
 	@Given("I Navigate to Login page")
 	public void i_navigate_to_login_page() {
 		
-		System.out.println("loginSuucssfull: ");
+
+		System.out.println("Create the browser ");
+		System.setProperty("webdriver.chrome.drive",
+				"C:\\Users\\rabiu\\OneDrive\\Documents\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("https://omayo.blogspot.com/");
 	  
 	}
 
@@ -19,7 +30,7 @@ public class Login {
 
 	@When("I entered the valid password")
 	public void i_entered_the_valid_password() {
-		System.out.println("passerf ");
+		System.out.println("Enter the correct password:  ");
 	    
 	}
 
@@ -31,7 +42,10 @@ public class Login {
 
 	@Then("I should get successful Message")
 	public void i_should_get_successful_message() {
-		System.out.println("Successfuly validated  ");
+		
+		driver.quit();
+		
+		System.out.println("Succssfully close the browser: ");
 	
 	}
 
